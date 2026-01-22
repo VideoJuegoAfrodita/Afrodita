f75d51cee2811fa07487a6561b1f924d2a7b26a7
Alexis <Yonathan.yp20@gmail.com>
2026-01-22 12:28:21 -0500

    copia branch yona

--- js/screens/end.js
+++ js/screens/end.js
@@ -1,12 +1,35 @@
+// screens/end.js
 export function renderEnd(score, isWin) {
   const app = document.getElementById("app");
 
   app.innerHTML = `
-    <section class="end-screen">
+    <section class="end-screen ${isWin ? "win" : "lose"}">
+      
+      <div class="heart-container">
+        ${
+          isWin
+            ? `
+              <div class="heart beating"></div>
+              <div class="mini-hearts">
+                <span>💗</span><span>💖</span><span>💕</span>
+              </div>
+            `
+            : `
+              <div class="broken-heart">
+                <div class="half left"></div>
+                <div class="half right"></div>
+              </div>
+            `
+        }
+      </div>
+
       <h2>${isWin ? "🎉 Victoria" : "💔 Derrota"}</h2>
       <p>Puntaje final: <strong>${score}</strong></p>
-      <button  id="play-again">Reintentar</button>
-      <button  id="go-home">Inicio</button>
+
+      <div class="end-buttons">
+        <button id="play-again">Reintentar</button>
+        <button id="go-home">Inicio</button>
+      </div>
     </section>
   `;
 
